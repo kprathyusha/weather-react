ece9d237619d399930bddbd727587a51a53cfa41
kprathyusha <pratyu.kulkarni@gmail.com>
2022-04-07 12:35:14 -0400

    updated Forecast day and included 5 day forecast

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,15 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./Forecast.css";
+import ForecastDay from "./ForecastDay";
 
 export default function Forecast(props) {
     let [loaded, setLoaded] = useState(false);
     let [forecast, setForecast] = useState(null);
 
-    const apiKey = "f64f24c2cb65bc7a2a8ea12b29366908";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${props.coordinates.lat}&lon=${props.coordinates.lon}&appid=${apiKey}&units=metric`;
-    console.log(apiUrl);
-    axios.get(apiUrl).then(handleResponse);
+    useEffect(() => {
+        setLoaded(false);
+    }, [props.coordinates]);
+    function load() {
+        const apiKey = "f64f24c2cb65bc7a2a8ea12b29366908";
+        let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${props.coordinates.lat}&lon=${props.coordinates.lon}&appid=${apiKey}&units=metric`;
+        console.log(apiUrl);
+        axios.get(apiUrl).then(handleResponse);
+    }
 
     function handleResponse(response) {
         //console.log(response.data.daily);
@@ -22,24 +28,22 @@ export default function Forecast(props) {
         return (
             <div className="Forecast">
                 <div className="row">
-                    <div className="col">
-                        <h6>{forecast[0].dt}</h6>
-                        <img
-                            src={`https://openweathermap.org/img/wn/${forecast[0].weather[0].icon}.png`}
-                            alt="clear"
-                            width="45"
-                        />
-                        <p>
-                            <span className="max-temperature">
-                                {forecast[0].temp.max}°
-                            </span>
-                            <span className="min-temperature">
-                                {forecast[0].temp.min}°
-                            </span>
-                        </p>
-                    </div>
+                    {forecast.map(function (dailyForecast, index) {
+                        if (index > 0 && index < 7) {
+                            return (
+                                <div className="col">
+                                    <ForecastDay data={dailyForecast} />
+                                </div>
+                            );
+                        } else {
+                            return null;
+                        }
+                    })}
                 </div>
             </div>
         );
+    } else {
+        load();
+        return null;
     }
 }
---
+++ src/ForecastDay.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function ForecastDay(props) {
+    function day() {
+        let date = new Date(props.data.dt * 1000);
+        let day = date.getDay();
+
+        let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+        return days[day];
+    }
+
+    return (
+        <div>
+            <h6>{day()}</h6>
+            <img
+                src={`https://openweathermap.org/img/wn/${props.data.weather[0].icon}.png`}
+                alt="clear"
+                width="45"
+            />
+            <p>
+                <span className="max-temperature">{props.data.temp.max}°</span>
+                <span className="min-temperature">{props.data.temp.min}°</span>
+            </p>
+        </div>
+    );
+}
